f165dbf1f705daba91fb0d00e9c87c7e495c220e
bonfaceotieno <bonnyotieno9@gmail.com>
2023-02-26 13:25:29 +0300

    Feat: Added UsernameAndPasswordAuthenticationFilter to security config

--- src/main/java/com/bonnieapps/springsecurityapp2/security/ApplicationSecurityConfig.java
+++ src/main/java/com/bonnieapps/springsecurityapp2/security/ApplicationSecurityConfig.java
@@ -2,17 +2,22 @@ package com.bonnieapps.springsecurityapp2.security;
 
 
 import com.bonnieapps.springsecurityapp2.auth.ApplicationUserService;
+import com.bonnieapps.springsecurityapp2.jwt.JwtUsernameAndPasswordAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -41,38 +46,21 @@ public class ApplicationSecurityConfig {
          * */
         http
                 .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // keeps the session stateless since it's using JWT for auth
+                .and()
+                .addFilter(new JwtUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeHttpRequests()  // authorize requests
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name()) //role based authentication
                 .anyRequest()
-                .authenticated() //any request must be authenticated i.e. client must specify the username and passwd
-                .and()
-                .formLogin() // and the mechanism that we want to reinforce the authenticity of the client is by using FormLogin
-                    .loginPage("/login").permitAll()
-                    .defaultSuccessUrl("/courses", true)
-                    .passwordParameter("password")
-                    .usernameParameter("username")
-                .and()
-                .rememberMe()//defaults to 2 weeks
-                    .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-                    .key("somethingverysecured")
-                    .rememberMeParameter("remember-me")
-                .and()
-                .logout()
-                    .logoutUrl("/logout")
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET")) //to prevent CSRF if CSRF is disabled,
-                    //and it means that when ever a client goes to that method with /logout url then log out
-                    .clearAuthentication(true)
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID", "remember-me")
-                    .logoutSuccessUrl("/login");
+                .authenticated(); //any request must be authenticated i.e. client must specify the username and passwd
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager() throws Exception {
+        return new ProviderManager(authenticationProvider());
     }
 
     @Bean
